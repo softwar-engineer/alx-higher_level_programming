f50a6a6e7d5b0c490a59cf1e09cfa87a28a1b503
Nebiyou Yemam <nebiyou.yemam@yahoo.com>
2023-03-08 05:28:36 -0800

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-# 7-islower.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-
 def islower(c):
     """Check for lowercase characters."""
     if ord(c) >= 97 and ord(c) <= 122:
